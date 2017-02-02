56f760880ff9de2291efbe1ecd66420123f07439
Zdravko Zdravkov <zdravko@tlb.uni-sofia.bg>
2017-02-02 19:13:11 +0200

    Changed commands order to UP container properly

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#Access from all over --- NEVER DO THIS SHIT IN POST DEV ENVs !!!!!!!!!!!!!!!!!!! <--- READ THIS 
-# cp /postgresql.conf /var/lib/pgsql/9.5/data/postgresql.conf
-# cp /pg_hba.conf /var/lib/pgsql/9.5/data/pg_hba.conf
+#Initialize data for pg engine
+/usr/pgsql-9.5/bin/postgresql95-setup initdb
+
+#Access from all over --- NEVER DO THIS SHIT IN POST DEV ENVs !!!!!!!!!!!!!!!!!!! <--- READ THIS
+cp /postgresql.conf /var/lib/pgsql/9.5/data/postgresql.conf
+cp /pg_hba.conf /var/lib/pgsql/9.5/data/pg_hba.conf
 
 # Update data folder perms
-# chown -R postgres.postgres /var/lib/pgsql
-
-#Initialize data for pg engine
-# /usr/pgsql-9.5/bin/postgresql95-setup initdb
+chown -R postgres.postgres /var/lib/pgsql
 
 # Start postgres daemon
-# exec su postgres -c "/usr/pgsql-9.5/bin/postgres -D /var/lib/pgsql/9.5/data"
+exec su postgres -c "/usr/pgsql-9.5/bin/postgres -D /var/lib/pgsql/9.5/data"
 
 # Update default postgres user password
-# exec su postgres -c "psql -c \"alter user postgres password 'password'\";"
-
-exec /bin/bash
+exec su postgres -c "psql -c \"alter user postgres password 'password'\";"
